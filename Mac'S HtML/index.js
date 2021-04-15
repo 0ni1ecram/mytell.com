00997efdfdd6490530e916ed708580f70a6d91c3
Ndewawiyo <71815468+0ni1ecram@users.noreply.github.com>
2021-04-15 23:04:14 +0300

    Add files via upload

---
+++ Mac'S HtML/index.js
@@ -0,0 +1,16 @@
+//alert("It is not Done yet this is as far as I could do!");
+
+//let name = "Marcel";
+//alert (`hello! ${7+4}`);
+
+/*
+alert ("Answer the following questions");
+let age = prompt ("How old are you?", 100);
+
+alert (`You are ${age} years old.`); 
+
+let youAreIOldEnough = confirm ("You are old enough!");
+alert(youAreIOldEnough);
+*/
+let value = true;
+alert (typeof value);
